3d03a12924a5e47e940580b9a367065ac146785c
陳泓安 <alpschen05@gmail.com>
2018-07-01 16:07:16 +0800

    color

--- screens/login/login.js
+++ screens/login/login.js
@@ -180,7 +180,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#FFDAB9"
+    backgroundColor: "#FFF"
   },
   textInput: {
     height: 40,
